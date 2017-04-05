5d0f2ee60024512f8dc1ac58539c0d40c4f3a624
Caitlin Hines and Katy Daviscourt <caitlin+katy@>
2017-04-05 11:24:56 -0700

    add detail page and routing- works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    ProjectDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/project.model.ts
@@ -0,0 +1,7 @@
+export class Project {
+
+  public donations: number = 0;
+
+  constructor (public title: string, public starter: string, public description: string, public goal: number, public plans: string, public swag: string, public id: number) { }
+
+}
