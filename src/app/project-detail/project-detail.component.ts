dfa6a6d64e37891aa75cedd44d2d1db36545cecf
Caitlin Hines and Katy Daviscourt <caitlin+katy@>
2017-04-05 11:38:41 -0700

    add fat arrow to foreach project loop

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Project } from '../project.model';
 
 @Component({
   selector: 'app-project-detail',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDetailComponent implements OnInit {
 
-  constructor() { }
+  projectId: number = null;
+
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.projectId = parseInt(urlParameters['id']);
+    });
   }
 
 }
